6fb48270d7f6a2368e865df299ceb73e1d5f8383
fabianhaef <fabian.haefi@gmail.com>
2026-07-27 19:29:11 +0200

    Add CP payment panel with policy-aware refunds (#38)

--- tests/Unit/Controllers/ControllerReflectionTest.php
+++ tests/Unit/Controllers/ControllerReflectionTest.php
@@ -155,7 +155,7 @@ class ControllerReflectionTest extends TestCase
             'cp/LocationsController' => ['anvildev\booked\controllers\cp\LocationsController', 3],
             'cp/BlackoutDatesController' => ['anvildev\booked\controllers\cp\BlackoutDatesController', 5],
             'cp/EventDatesController' => ['anvildev\booked\controllers\cp\EventDatesController', 6],
-            'cp/BookingsController' => ['anvildev\booked\controllers\cp\BookingsController', 10],
+            'cp/BookingsController' => ['anvildev\booked\controllers\cp\BookingsController', 11],
             'cp/SettingsController' => ['anvildev\booked\controllers\cp\SettingsController', 14],
             'cp/WaitlistController' => ['anvildev\booked\controllers\cp\WaitlistController', 6],
             'cp/WebhooksController' => ['anvildev\booked\controllers\cp\WebhooksController', 9],
